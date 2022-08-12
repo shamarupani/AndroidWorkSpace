61f7f502bf975bf2d17fefb041333b85e118f29d
shamarupani <109715231+shamarupani@users.noreply.github.com>
2022-08-12 14:16:35 +0530

    Update InheritanceEx.java

--- Module 7/Practice/InheritanceEx.java
+++ Module 7/Practice/InheritanceEx.java
@@ -1,3 +1,11 @@
+/*
+B38. Create a class with a method that prints “This is a parent class” and its subclass with another
+method that prints “This is child class”. Now, create an object for each of the class and call
+1 - method of parent class by object of parent class
+2 - method of child class by object of child class
+3 - method of parent class by object of child class
+*/
+
 package task128;
 
 class Parent
