3976264701bd9ba582767360079cf9a14840b46a
shamarupani <109715231+shamarupani@users.noreply.github.com>
2022-08-12 14:13:41 +0530

    Update OverridingEx2.java

--- Module 7/Practice/OverridingEx2.java
+++ Module 7/Practice/OverridingEx2.java
@@ -1,3 +1,10 @@
+/*
+B36. Create a class to print an integer and a character with two methods having the same name but
+different sequence of the integer and the character parameters.
+For example, if the parameters of the first method are of the form (int n, char c), then that of the
+second method will be of the form (char c, int n).
+*/
+
 package task128;
 
 class Sequence
