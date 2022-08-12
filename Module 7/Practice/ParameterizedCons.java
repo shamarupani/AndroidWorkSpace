6d43554e9c9c1ce2c0858a6f548ee94f43091ac0
shamarupani <109715231+shamarupani@users.noreply.github.com>
2022-08-12 14:18:06 +0530

    Update ParameterizedCons.java

--- Module 7/Practice/ParameterizedCons.java
+++ Module 7/Practice/ParameterizedCons.java
@@ -1,3 +1,12 @@
+/*
+B40. Create a class named Rectangle with two data members length and breadth and two methods to
+print the area and perimeter of the rectangle respectively. Its constructor having parameters for
+length and breadth is used to initialize the length and breadth of the rectangle. Let class Square
+inherit the Rectangle class with its constructor having a parameter for its side (suppose s) calling the
+constructor of its parent class as ‘super (s, s)’. Print the area and perimeter of a rectangle and a
+square.
+*/
+
 package task128;
 
 class Rectangle
