fc7ee611f34986cd2a7cc44b04608f4ff5148251
shamarupani <109715231+shamarupani@users.noreply.github.com>
2022-08-13 12:28:54 +0530

    Update Exception1.java

--- Module 7/Practice/Exception1.java
+++ Module 7/Practice/Exception1.java
@@ -1,3 +1,9 @@
+/*
+I1. W.A.J.P to read 10 integer values using Scanner class from keyboard. Generate the exception if
+the entered value is less than 10 and or greater than 40. Program should display appropriate message
+as and when this exception occurs otherwise it will display all the entered values.
+*/
+
 package a138;
 
 import java.util.Scanner;
