a5bb33bd95d32812e331cdc6593369ce7614db2e
shamarupani <109715231+shamarupani@users.noreply.github.com>
2022-08-13 12:29:30 +0530

    Update Vote.java

--- Module 7/Practice/Vote.java
+++ Module 7/Practice/Vote.java
@@ -1,3 +1,9 @@
+
+/*
+I2. W.A.J.P to create the validate method that takes integer value as a parameter. If the age is less
+than 18, then throw an ArithmeticException otherwise print a message welcome to vote.
+*/
+
 package a138;
 
 
