5dc9923abdada7a66873212789d7aebf02a7d694
shamarupani <109715231+shamarupani@users.noreply.github.com>
2022-09-14 13:35:48 +0530

    Add files via upload

---
+++ Module 3/14.9.22/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.myapplicationnew
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean
+    {
+        menuInflater.inflate(R.menu.option,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean
+    {
+        when(item.itemId)
+        {
+            R.id.i1->
+            {
+                var num="999999999"
+                var i=Intent(Intent.ACTION_CALL)
+                i.setData(Uri.parse("tel:"+num))
+                startActivity(i)
+            }
+            R.id.i2->
+            {
+                Toast.makeText(this,"About Us",Toast.LENGTH_LONG).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        menuInflater.inflate(R.menu.context,menu)
+        super.onCreateContextMenu(menu, v, menuInfo)
+    }
+
+}
