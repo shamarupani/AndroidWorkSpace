b36ef2ab9ccf625502ac74c1ba39e221c6f00091
Shama Rupani <109715231+shamarupani@users.noreply.github.com>
2022-12-20 13:33:35 +0530

    Delete Module 3/21.9.22 directory

--- Module 3/21.9.22/Assignment/Model.kt
+++
@@ -1,7 +0,0 @@
-package com.example.foodfeed
-
-import android.widget.ImageView
-
-class Model(var image:Int,var fooditem:String,var resname:String,var price:String,var ratings:String)
-{
-}
--- Module 3/21.9.22/CustomList/Model.kt
+++
@@ -1,5 +0,0 @@
-package com.example.customlist
-
-class Model(var image:Int,var name:String,var des:String)       //Constructor
-{
-}
